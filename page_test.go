002b30277963da0fd5a1decd2363090f404a8926
lemmi <lemmi@nerd2nerd.org>
2015-08-27 00:18:30 +0200

    add basic tests

--- page_test.go
+++ page_test.go
@@ -1,34 +1,31 @@
 package glubcms
 
 import (
+	"fmt"
 	"html/template"
-	"io/ioutil"
 	"os"
 	"testing"
 )
 
-func TestPage(t *testing.T) {
-	p := Page{
-		Menu: [][]MenuEntry{
-			{
-				{Entry{title: "Menu 11"}},
-				{Entry{title: "Menu 12"}},
-				{Entry{title: "Menu 13", active: true}},
-			},
-			{
-				{Entry{title: "Menu 11", active: true}},
-				{Entry{title: "Menu 12"}},
-			},
-		},
-		Articles: []Article{
-			{Entry: Entry{title: "Article 1", active: true}, html: []byte{}},
-			{Entry: Entry{title: "Article 2"}, html: []byte{}},
-			{Entry: Entry{title: "Article 3"}, html: []byte{}},
-		},
-	}
-
+func TestArticlePage(t *testing.T) {
+	p := PageFromDir("example_page", "menu1/article1")
+	fmt.Println(p.Outline())
+}
+func TestMenuPage(t *testing.T) {
+	p := PageFromDir("example_page", "menu1/")
+	fmt.Println(p.Outline())
+}
+func TestLandingPage(t *testing.T) {
+	p := PageFromDir("example_page", "")
+	fmt.Println(p.Outline())
+}
+func TestEntry(t *testing.T) {
+	e := entryFromDir("example_page/", "menu1/article1", "menu1/article1").(*entry)
+	fmt.Printf("%+v", e)
+	fmt.Printf("%+v", string(e.HTML()))
+}
+func TestTemplate(t *testing.T) {
 	tmpl := template.Must(template.ParseFiles("page_template.html"))
-	page := p.RenderHTML(tmpl)
-	os.Stdout.Write(page)
-	ioutil.WriteFile("/tmp/page.html", page, 0600)
+	p := PageFromDir("example_page", "menu1/article1")
+	fmt.Println(tmpl.Execute(os.Stdout, p))
 }
