3d347175362d8c1ce0778b9432e7dd14be2348e4
lemmi <lemmi@nerd2nerd.org>
2015-09-22 01:44:08 +0200

    gcserver: reroute favicon.ico, robots.txt to /static

--- gcserver/main.go
+++ gcserver/main.go
@@ -35,10 +35,10 @@ func main() {
 	prefix := flag.String("prefix", "../example_page", "path to the root dir")
 	addr := flag.String("bind", "localhost:8080", "address to bind to")
 	flag.Parse()
-	http.Handle("/static/",
-		http.StripPrefix("/static/",
-			http.FileServer(http.Dir(
-				filepath.Join(*prefix, "static")))))
+	staticHandler := http.FileServer(http.Dir(filepath.Join(*prefix, "static")))
+	http.Handle("/static/", http.StripPrefix("/static/", staticHandler))
+	http.Handle("/robots.txt", staticHandler)
+	http.Handle("/favicon.ico", staticHandler)
 	http.Handle("/", newhandler(*prefix))
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
