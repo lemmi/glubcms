f8eb01c980a165f016b86427d2fb5790967b0384
lemmi <lemmi@nerd2nerd.org>
2015-10-14 21:22:01 +0200

    update tests

--- page_test.go
+++ page_test.go
@@ -3,29 +3,30 @@ package glubcms
 import (
 	"fmt"
 	"html/template"
+	"net/http"
 	"os"
 	"testing"
 )
 
 func TestArticlePage(t *testing.T) {
-	p := PageFromDir("example_page", "menu1/article1")
+	p := PageFromDir(http.Dir("example_page/pages"), "menu1/article1")
 	fmt.Println(p.Outline())
 }
 func TestMenuPage(t *testing.T) {
-	p := PageFromDir("example_page", "menu1/")
+	p := PageFromDir(http.Dir("example_page/pages"), "menu1/")
 	fmt.Println(p.Outline())
 }
 func TestLandingPage(t *testing.T) {
-	p := PageFromDir("example_page", "")
+	p := PageFromDir(http.Dir("example_page/pages"), "")
 	fmt.Println(p.Outline())
 }
 func TestEntry(t *testing.T) {
-	e := entryFromDir("example_page/", "menu1/article1", "menu1/article1").(*entry)
+	e := entryFromDir(http.Dir("example_page/pages"), "menu1/article1", "menu1/article1").(*entry)
 	fmt.Printf("%+v", e)
 	fmt.Printf("%+v", string(e.HTML()))
 }
 func TestTemplate(t *testing.T) {
-	tmpl := template.Must(template.ParseFiles("page_template.html"))
-	p := PageFromDir("example_page", "menu1/article1")
+	tmpl := template.Must(template.ParseGlob("example_page/templates/*tmpl"))
+	p := PageFromDir(http.Dir("example_page/pages"), "menu1/article1")
 	fmt.Println(tmpl.Execute(os.Stdout, p))
 }
